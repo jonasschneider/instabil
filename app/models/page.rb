08063a277cc7a17d05bae9f72703b378d3727533
Jonas Schneider <mail@jonasschneider.com>
2011-12-01 21:36:19 +0100

    start making pages independent of the person

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,43 +3,22 @@ class Page
   include Mongoid::Versioning
   include Mongoid::Timestamps
   
-  field :kurs, type: Integer
-  field :g8, type: Boolean
-  
-  field :lks, type: String
-  field :bio, type: String
-  #field :foto, type: String
-  
   field :text, type: String
-  
-  field :tags, type: Array
   
   belongs_to :author, class_name: 'Person', inverse_of: nil
   validates_presence_of :author
   
-  belongs_to :person
+  has_one :person
   
   def api_attributes
-    {}.tap do |att|
-      fields.keys.each do |field|
-        next if field[0] == '_'
-        if field.to_s == 'g8'
-          att[field] = read_attribute(field) ? 1 : 0
-        else
-          att[field] = read_attribute(field)
-        end
-      end
-      att['author'] = self.author && author.name
-      att['foto'] = nil
-    end
+    { 
+      text: self.text,
+      author: (self.author && author.name)
+    }
   end
   
   def date
     updated_at || created_at
-  end
-  
-  def zug
-    g8 ? 'G8' : 'G9'
   end
   
   def get_version_attributes(num)
