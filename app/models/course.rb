d53ffd2f85bcab7396b66d241c7686b4dbdd3db0
Jonas Schneider <mail@jonasschneider.com>
2011-12-02 23:38:13 +0100

    extract data from course name

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,11 @@ class Course
   
   field :name
   belongs_to :page
+  
+  def group
+    return nil if name.nil?
+    num, subject = $1, $2 if name.match(/^(\d+)([^\d]+)\d+/)
+    t = num.to_i == 4 ? 'vierstündig' : 'zweistündig'
+    "#{subject.capitalize} (#{t})"
+  end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,7 +1,15 @@
 require File.expand_path(File.dirname(__FILE__)+'/../spec_helper')
 
 describe "Course" do
-  let(:course) { Course.create! name: '4Bi2' }
+  let(:course) { Course.create! name: '4BI2' }
+  
+  it "returns the #group" do
+    course.group.should == 'Bi (vierstündig)'
+    course.name = '2INF05'
+    course.group.should == 'Inf (zweistündig)'
+    course.name = nil
+    course.group.should == nil
+  end
   
   it "is valid" do
     course.should be_valid
