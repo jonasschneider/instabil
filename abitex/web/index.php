6b5f920fa63bf82b9d834c5ce26a569f30531792
Lukas K <lu@0x83.eu>
2011-12-04 21:39:38 +0100

    send content-length

--- abitex/web/index.php
+++ abitex/web/index.php
@@ -9,14 +9,16 @@ $timestamp_sig = $_GET["timestamp_sig"];
 #echo "sig=" . $timestamp_sig . "<br />";
 if(abs(time() - $timestamp) > 30) {
 	#die("Zeit abgelaufen; es ist " . time());
-	die("Zeit abgelaufen, klick auf <a href='http://instabil.heroku.com'>instABIl</a> auf den Link zum PDF.");
+	die("Zeit abgelaufen, <a href='http://instabil.heroku.com/current_pdf'>versuch's nochmal</a>");
 }
 if(hash_hmac("sha1", strval($timestamp), $secret) != $timestamp_sig) {
 	die("Falsche signatur");
 }
 header('Content-type: application/pdf');
 header('Content-Disposition: attachment; filename="abi.pdf"');
-@readfile("abi.pdf");
+header("Content-Transfer-Encoding: binary"); 
+header("Content-Length: ".filesize("ftp/abi.pdf")); 
+@readfile("ftp/abi.pdf");
 #echo hmac_sha1(1, 1);
 
 ?>
