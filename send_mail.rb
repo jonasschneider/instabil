eff55a7d398e328fb47779913bf08a5f0083c942
Jonas Schneider <mail@jonasschneider.com>
2011-12-16 18:33:04 +0100

    don't fail on errors

--- send_mail.rb
+++ send_mail.rb
@@ -1,4 +1,4 @@
-require "pony"
+require "mail"
 require "ap"
 require 'open-uri'
 require "json"
@@ -16,11 +16,27 @@ puts "Done, got #{people.length} records.".green
 print "Subject: ".white
 SUBJECT = $stdin.gets
 
-puts "Body (until EOF):".white
-BODY = $stdin.read
+print "Body filename: ".white
+filename = $stdin.gets.strip
+BODY = File.read(filename)
+puts BODY.encoding
 
 puts "Press Enter to continue.".yellow
 $stdin.gets
+
+options = {
+  :address              => 'smtp.gmail.com',
+  :port                 => '587',
+  :enable_starttls_auto => true,
+  :user_name            => ENV['GMAIL_SMTP_USER'],
+  :password             => ENV['GMAIL_SMTP_PASSWORD'],
+  :authentication       => :plain,
+  :domain               => ENV['GMAIL_SMTP_USER'] # the HELO domain provided by the client to the server
+}
+
+Mail.defaults do
+  delivery_method :smtp, options
+end
 
 puts
 sent = 0
@@ -30,22 +46,18 @@ people.each do |person|
     puts "skipping".yellow
     next
   end
-  
-  Pony.mail({
-    :to => person["email"],
-    :subject => SUBJECT,
-    :body => BODY,
-    :via => :smtp,
-    :via_options => {
-      :address              => 'smtp.gmail.com',
-      :port                 => '587',
-      :enable_starttls_auto => true,
-      :user_name            => ENV['GMAIL_SMTP_USER'],
-      :password             => ENV['GMAIL_SMTP_PASSWORD'],
-      :authentication       => :plain,
-      :domain               => ENV['GMAIL_SMTP_USER'] # the HELO domain provided by the client to the server
-    }
-  })
+  begin
+    m = Mail.new({
+      :from => ENV['GMAIL_SMTP_USER'],
+      :to => person["email"],
+      :subject => SUBJECT,
+      :body => BODY,
+      :charset => "utf-8"
+    })
+    m.deliver!
+  rescue Exception => e
+    puts e.message.red
+  end
   puts "sent".green
   sent += 1
 end
