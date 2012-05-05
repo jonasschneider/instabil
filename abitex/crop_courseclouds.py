5c9c7478c3609e3b0bb6a091cda6752add72426d
Jonas Schneider <mail@jonasschneider.com>
2012-05-06 00:01:32 +0200

    only update modified clouds

--- abitex/crop_courseclouds.py
+++ abitex/crop_courseclouds.py
@@ -10,11 +10,14 @@ j = json.loads(blah)
 
 for course in j :
   id = course['id']
+  infile = 'linked/courses/raw_clouds/'+id+'.png'
+  outfile = 'linked/courses/clouds/'+id+'.png'
 
   print id
 
-  im = Image.open('linked/courses/raw_clouds/'+id+'.png')
-  s = im.size
-  box = (30, 30, im.size[0]-30, im.size[1]-100)
-  region = im.crop(box)
-  region.save('linked/courses/clouds/'+id+'.png')
+  if not os.path.isfile(outfile) or os.stat(infile).st_mtime > os.stat(outfile).st_mtime:
+    im = Image.open(infile)
+    s = im.size
+    box = (30, 30, im.size[0]-30, im.size[1]-100)
+    region = im.crop(box)
+    region.save(outfile)
