37194c1b326ab7f177add55635d7b82a6f81cdbe
Jonas Schneider <mail@jonasschneider.com>
2012-05-06 12:32:53 +0200

    group courses by type and subject

--- abitex/buildcourses.py
+++ abitex/buildcourses.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python2
-
-import json, os, subprocess
+# -*- coding: utf-8 -*-
+import json, os, subprocess, itertools
 from string import Template
 f = open("courses.json")
 blah = f.read()
 j = json.loads(blah)
 temp = open("temp/course.tex").read()
-courselist = []
 n = 0
+
+def escape_tex(text) :
+	return text.replace(u"♥", "<3").replace(u"☺", " :) ").replace("&#3232;", u"{\\Tunga ಠ}").replace("&", "\\&").replace("#", "\\#").replace("_", "\\_").replace("^", "\^{}").replace(u"%", "\%").replace(u"✚", u"{\\DjVu ✚}").replace(u"‿", u"{\\DjVu ‿}").replace(u"✿", u"{\\DjVu ✿}").replace(u"ё", '"e').replace(u"§nl§", u"\\\\").replace("\\textbackslash\\/LaTeX", "\\LaTeX").replace("\\textbackslash\\/vspace", "\\vspace").replace("\\m/", "\\textbackslash m/")
+
 for course in j :
 	n+=1
 	page = Template(temp.decode("utf-8"))
 	#if course["author"] == "" :
 	#	continue
-	courselist.append(course["id"])
 	print course["id"]
 	#print "tex/pupils/" + pupil["uid"] + ".tex"
 	#print type(content["tags"])
@@ -27,10 +29,21 @@ for course in j :
 		course["type"] = "zwei"
 	proc = subprocess.Popen("./md2tex.sh", stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 	course["text"] = proc.communicate(course["text"].encode("utf-8"))[0].decode("utf-8")
+
+	course["author"] = escape_tex(course["author"])
+	
 	out = page.substitute(course)
 	f =  open("tex/courses/" + course["id"] + ".tex", "w")
 	f.write(out.encode("utf-8"))
-print "%i Kurse, %i berichte"%(n, len(courselist))
+print "%i Kurse"%n
 f =  open("tex/courses.tex", "w")
-for p in courselist :
-	f.write("\input{courses/" + p + ".tex}\n");
+
+getnum = lambda s:s['num']
+getsubject = lambda s:s['fach']
+
+sorted_input = reversed(sorted(j, key=getnum))
+for num,courses in itertools.groupby(sorted_input, key=getnum):
+	f.write("\section{%d-stündige Kurse}\n"%num);
+	sorted_by_subject = sorted(courses, key=getsubject)
+	for course in sorted_by_subject:
+		f.write("\input{courses/" + course["id"] + ".tex}\n");
