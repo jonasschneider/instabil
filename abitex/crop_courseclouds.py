dced2fd9c5ff7d86f14b96e3b4871e05eb90e210
Jonas Schneider <mail@jonasschneider.com>
2012-05-07 21:20:10 +0200

    compress course pages to make space for name list

--- abitex/crop_courseclouds.py
+++ abitex/crop_courseclouds.py
@@ -21,26 +21,26 @@ for course in j :
 
   print id
 
-  if not os.path.isfile(outfile) or os.stat(infile).st_mtime > os.stat(outfile).st_mtime:
-    im = Image.open(infile)
-    im = im.convert('L')
-    s = im.size
-    box = (50, 50, im.size[0]-50, im.size[1]-100)
-    noborder = im.crop(box)
-    bg = Image.new(noborder.mode, noborder.size, 'black')
-    mask = ImageChops.add(noborder, bg, 1, 100)
-    mask = ImageChops.invert(mask)
-    bbox = mask.getbbox()
-    if bbox == None:
-      realbox = 0, 0, noborder.size[0]-1, 100
-    else:
-      realbox = 0, bbox[1]-40, noborder.size[0]-1, bbox[3]+40
-    print bbox, realbox
-    trimmed = noborder.crop(realbox)
+  #if not os.path.isfile(outfile) or os.stat(infile).st_mtime > os.stat(outfile).st_mtime:
+  im = Image.open(infile)
+  im = im.convert('L')
+  s = im.size
+  box = (50, 50, im.size[0]-50, im.size[1]-100)
+  noborder = im.crop(box)
+  bg = Image.new(noborder.mode, noborder.size, 'black')
+  mask = ImageChops.add(noborder, bg, 1, 100)
+  mask = ImageChops.invert(mask)
+  bbox = mask.getbbox()
+  if bbox == None:
+    realbox = 0, 0, noborder.size[0]-1, 100
+  else:
+    realbox = 0, bbox[1]-40, noborder.size[0]-1, bbox[3]
+  print bbox, realbox
+  trimmed = noborder.crop(realbox)
 
-    mask = ImageChops.add(trimmed, bg, 1, 100)
-    mask = ImageChops.invert(ImageEnhance.Contrast(mask).enhance(30.0))
+  mask = ImageChops.add(trimmed, bg, 1, 100)
+  mask = ImageChops.invert(ImageEnhance.Contrast(mask).enhance(30.0))
 
-    if not common.drafting():
-      trimmed.putalpha(mask)
-    trimmed.save(outfile)
+  if not common.drafting():
+    trimmed.putalpha(mask)
+  trimmed.save(outfile)
