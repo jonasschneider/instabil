7092b3820d6d2f3c78c43cb8924b07948812c6de
Lukas K <lu@0x83.eu>
2012-05-17 21:24:58 +0200

    widmeral, kramerlu

--- abitex/buildpupils.py
+++ abitex/buildpupils.py
@@ -40,7 +40,7 @@ verysmallnames = ("leikerch", "meissnna")
 
 sizes = ("\\scriptsize", "\\kathisize", "\\footnotesize", "\\small")
 
-smallmeta = ("elgogoli", "ingentjo", "mahlerda", "weberha", "werrnal", "wolffrda", "reinhaca", "kramerlu", "meissnna", "mercanze", "winteran", "broderti")
+smallmeta = ("elgogoli", "ingentjo", "mahlerda", "weberha", "werrnal", "wolffrda", "reinhaca", "kramerlu", "meissnna", "mercanze", "winteran", "broderti", "blumesi")
 verysmallmeta = ("weilerki")
 
 print 'Namen zu lang:'
